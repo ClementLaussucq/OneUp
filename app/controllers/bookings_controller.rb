fb7a2e5a41ce0c5761ca0f22b6eed567c560ada6
christophe bouquet <christophe.productowner@gmail.com>
2019-05-24 14:52:28 +0200

    bookingcontroller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,22 @@
 class BookingsController < ApplicationController
+  before_action :current_booking, only: [:show, :edit, :update, :destroy]
   def index
+    @bookings = Booking.all
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @experience = Experience.find(params[:experience_id])
+    @booking = Booking.new(booking_params)
+    @booking.experience = @experience
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -15,8 +26,23 @@ class BookingsController < ApplicationController
   end
 
   def update
+    @booking.update(booking_params)
+
+    redirect_to booking_path(@booking)
   end
 
   def destroy
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :experience_id, :date, :status)
+  end
+
+  def current_booking
+    @booking = Booking.find(params[:id])
   end
 end
